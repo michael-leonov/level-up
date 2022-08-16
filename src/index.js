85b62d95726fd9c08d5e3a5e3eb015046d6b7b28
Michael <michael.leonov96@mail.ru>
2022-08-16 23:44:30 +0300

    pre-completed task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/style.scss';
-import Swiper from 'swiper';
-import '../node_modules/swiper/swiper.scss';
+import Swiper from 'swiper/bundle';
+import 'swiper/scss';
 
 // eslint-disable-next-line no-unused-vars
 const swiper = new Swiper('.swiper', {
@@ -9,15 +9,22 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 30,
     slidesPerGroup: 1,
     centeredSlides: true,
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    autoplay: {
+        delay: 5000,
     },
+    speed: 700,
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1.4,
+            spaceBetween: 30,
+            centeredSlides: true,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
     },
 });
