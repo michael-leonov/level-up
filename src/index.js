7e14312b3c665fe9783d512840685f1b97133571
michael-leonov <michael.leonov96@mail.ru>
2022-08-16 17:01:57 +0300

    intermediate changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles/style.scss';
+import Swiper from 'swiper';
+import '../node_modules/swiper/swiper.scss';
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
